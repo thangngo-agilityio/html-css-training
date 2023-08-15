b4d5ea95d0658d0d888dbbe5117899e0edfba203
thangngo <thangngo@asnet.com.vn>
2023-08-15 11:26:06 +0700

    edit file gitignore

---
+++ practice-one/dist/index.f3c17023.js
@@ -0,0 +1,2 @@
+window.onload=function(){let e=document.querySelector(".hamburger"),t=document.querySelector(".mobile-nav");e.addEventListener("click",function(){e.classList.toggle("is-active"),t.classList.toggle("is-active")})};
+//# sourceMappingURL=index.f3c17023.js.map
